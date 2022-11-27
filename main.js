7013a133ee836fa3794d5576d878eb185dc7131a
mushtariyibodillayeva <dilfuzaxamrayeva53@gmail.com>
2022-11-27 17:46:07 +0500

    slide

---
+++ main.js
@@ -0,0 +1,20 @@
+function changeColor1(newColor) {
+  const elem = document.getElementById("text1");
+  elem.style.color = newColor;
+}
+function changeColor2(newColor) {
+  const elem = document.getElementById("text2");
+  elem.style.color = newColor;
+}
+function changeColor3(newColor) {
+  const elem = document.getElementById("text3");
+  elem.style.color = newColor;
+}
+function changeColor4(newColor) {
+  const elem = document.getElementById("text4");
+  elem.style.color = newColor;
+}
+function changeColor5(newColor) {
+  const elem = document.getElementById("text5");
+  elem.style.color = newColor;
+}
